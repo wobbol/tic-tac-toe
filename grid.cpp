e9bdfb1a39a9467fea048358302f7c1b6c71060d
na-me <robwobbol@gmail.com>
2014-03-12 15:36:03 -0500

    moved grid functions into their proper place.

--- grid.cpp
+++ grid.cpp
@@ -1,3 +1,38 @@
+#include <iostream>
 #include "grid.h"
-
+using namespace std;
 int grid[NUMROW][NUMCOL];
+
+void printGrid(){
+int i,j;
+	for(i = 0; i < NUMROW; i++){
+
+		for(j = 0; j < NUMCOL; j++){
+		cout << grid[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+int editGrid(int i,int j,int player){ //ROW,COLLUM
+if( grid[i][j] == 1 || grid[i][j] == 2){
+	cout << "this space is full" << endl;
+	return 1;
+}
+grid[i][j] = player;
+return 0;
+}
+
+void clearGrid(){
+int i,j;
+	for(i = 0; i < NUMROW; i++){
+
+		for(j = 0; j < NUMCOL; j++){
+		grid[i][j] = 0;
+		}
+	}
+}
+
+
+
--- grid.h
+++ grid.h
@@ -4,4 +4,8 @@
 #define NUMCOL 3
 #define NUMROW 3
 extern int grid[NUMROW][NUMCOL];
+
+void printGrid();
+int editGrid(int i, int j, int player);
+void clearGrid();
 #endif
