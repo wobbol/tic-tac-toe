abdc8d38022d51375454bba5dbcd8e9b3464a363
na-me <robwobbol@gmail.com>
2014-03-12 15:41:39 -0500

    Housekeeing

--- main.cpp
+++ main.cpp
@@ -1,43 +1,12 @@
 #include <iostream>
 #include "winner.h"
 #include "grid.h"
+#include "game.h"
 using namespace std;
 
 
 #define PLAYER1 1
 #define PLAYER2	2
-
-
-
-void printGrid(){
-int i,j;
-	for(i = 0; i < NUMROW; i++){
-
-		for(j = 0; j < NUMCOL; j++){
-		cout << grid[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
-int editGrid(int i,int j,int player){ //ROW,COLLUM
-if( grid[i][j] == 1 || grid[i][j] == 2){
-	cout << "this space is full" << endl;
-	return 1;
-}
-grid[i][j] = player;
-return 0;
-}
-
-void clearGrid(){
-int i,j;
-	for(i = 0; i < NUMROW; i++){
-
-		for(j = 0; j < NUMCOL; j++){
-		grid[i][j] = 0;
-		}
-	}
-}
 
 
 void printMenu(){
@@ -67,22 +36,8 @@ int main(int argc, char** argv)
 	while(!quit){
 		cin >> input;
 		switch(input){
-		
 		case 'm':
-		  cout << "row?:";
-		  cin >> ++row;
-		  row = 0;
-		  cout << "colum?:";
-		  cin >> ++col;
-		  col = 0;
-		  		//TODO: like really this is asking frou trouble
-		  editGrid(row,col,player);
-		  if(player == PLAYER1){
-		  player = PLAYER2;
-		  } else{
-		  player = PLAYER1;
-		  }
-		  printGrid();
+		  game();
 		  break;
 
 		case 'p':
@@ -114,15 +69,15 @@ int main(int argc, char** argv)
 		}
 		cout << ">";
 	}
-	editGrid(0,1,PLAYER1);
-	editGrid(0,2,PLAYER2);
+	editGrid(0,0,PLAYER1);
+	editGrid(1,0,PLAYER1);
+	editGrid(2,0,PLAYER1);
 	editGrid(1,1,PLAYER2);
-	editGrid(2,0,PLAYER2);
-//	editGrid(0,0,PLAYER2);
+  	editGrid(2,2,PLAYER2);
 //	editGrid(1,1,PLAYER2);
 //	editGrid(2,2,PLAYER2);
-	if(winnerTest(PLAYER2));
-	cout << "player 2 wins!" << endl;
+	if(winnerTestCol(0,2))
+	  cout << "player 2 wins!" << endl;
 	printGrid();
 	return 0;
 }
