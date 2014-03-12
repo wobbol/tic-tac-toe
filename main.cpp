d4db1bb945e84571d0f84e5b671161cbac06ab56
na-me <robwobbol@gmail.com>
2014-03-11 23:57:00 -0500

    changed NUMROW and NUMCOL from #defines to ints

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-#define NUMCOL 3
-#define NUMROW 3
+int NUMCOL 3
+int NUMROW 3
 
 #define PLAYER1 1
 #define PLAYER2	2
@@ -40,60 +40,6 @@ int i,j;
 		}
 	}
 }
-
-bool winnerTestCol(int col,int player ){ //on 'player' not having a win in col returns 1
-int i;
-	for(i = 0; i < NUMROW; i++){
-		if(grid[i][col] != player){
-			return 0;
-		}
-		return 1;
-	}
-}
-bool winnerTestRow(int row,int player ){ //on 'player' not having a win in row returns 1
-int i;
-	for(i = 0; i < NUMCOL; i++){
-		if(grid[row][i] != player){
-			return 0;
-		}
-		return 1;
-	}
-}
-
-bool winnerTestDiagUp(int player){
-int i,j;
-	for(i = 0,j = 0; i < NUMROW; i++,j++){
-		if(grid[i][j] != player){
-			return 0;
-		}
-	}
-return 1;
-}
-bool winnerTestDiagDown(int player){
-int i,j;
-	for(i = NUMROW - 1, j = 0; i > 0; i--,j++){
-		if(grid[i][j] != player){
-			return 0;
-		}
-	}
-return 1;
-}
-
-bool winnerTest(int player){ //return true on 'player' win
-int i;
-	for(i = 0; i < NUMCOL; i++){
-		if(winnerTestCol(i,player))
-			return 1;
-	}
-	for(i = 0; i < NUMROW; i++){
-		if(winnerTestRow(i,player))
-			return 1;
-	}
-	if(winnerTestDiagUp(player))
-		return 1;
-	if(winnerTestDiagDown(player))
-		return 1;
-return 0;
 
 
 }
