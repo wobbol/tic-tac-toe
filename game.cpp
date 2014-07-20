226cef4062d79958bb80d984ae1de4db92188241
na-me <robwobbol@gmail.com>
2014-07-19 23:38:35 -0500

    game loop rough draft

--- game.cpp
+++ game.cpp
@@ -6,24 +6,39 @@ int game(){
 int row;
 int col;
 	while(1){
+	player1:
 		cout << "Player1>" << endl;
 		cout << "row:";
 		cin >> row;
-		cout << endl << "colum:";
+		cout << endl;
+		cout << "colum:";
 		cin >> col;
-		editGrid(row,col,1);
+		cout << endl;
+		if( movePlayer( row, col, 1 ) ) {
+			cout << "make a valid move" << endl;
+			goto player1;
+		}
 		printGrid();
 		if(!winnerTest(1)){
 		  return 1;
 		}
-	
+	player2:
 		cout << "Player2>" << endl;
 		cout << "row:";
 		cin >> row;
-		cout << endl << "colum:";
+		cout << endl;
+		cout << "colum:";
 		cin >> col;
-		editGrid(row,col,2);
+		cout << endl;
+		if( !movePlayer( row, col, 2 ) ) {
+			cout << "make a valid move" << endl;
+			goto player2;
+		}
 		printGrid();
+		if(!winnerTest(2)){
+		  return 2;
+		}
+
 	}
 return 0;
 }
