1b947a8e432e531a518728839b1c7fd3c079f415
na-me <robwobbol@gmail.com>
2014-07-21 01:57:45 -0500

    now with functioning winner tests!

--- winner.cpp
+++ winner.cpp
@@ -6,58 +6,88 @@
 #include <iostream>
 #include "grid.h"
 
-bool winnerTestCol(int col,int player ){ //on 'player' not having a win in col returns 1
+bool testCol(int col,int player ){ //on 'player' not having a win in col returns 1
 int i;
+int count = 0;
 	for(i = 0; i < NUMROW; i++){
-		if(grid[i][col] != player){
+		if(grid[i][col] == player){
+			count++;
+		}
+	}
+		if(count == 3){
+			return 1;
+		} else {
 			return 0;
 		}
-		return 1;
-	}
 }
-bool winnerTestRow(int row,int player ){ //on 'player' not having a win in row returns 1
+bool testRow(int row,int player ){ //on 'player' not having a win in row returns 1
 int i;
+int count = 0;
 	for(i = 0; i < NUMCOL; i++){
-		if(grid[row][i] != player){
-			return 0;
+		if(grid[row][i] == player){
+			count++;
 		}
+	}
+	if( count == 3){
 		return 1;
+	} else {
+		return 0;
 	}
 }
-
-bool winnerTestDiagUp(int player){ // best way to make this not edge alligned
-int i,j;				//would be accessing the grid as a
-for(i = 0,j = 0; i < NUMROW; i++,j++){ //1 dimentional array and using an offset
-	if(grid[i][j] != player){//with a multiple reliant of the size of the grid
-	return 0;
-	}
-	}
-return 1;
-}
-bool winnerTestDiagDown(int player){
-int i,j;
-	for(i = NUMROW - 1, j = 0; i > 0; i--,j++){
-		if(grid[i][j] != player){
-			return 0;
+/*
+ *best way to make this not edge alligned
+ *would be accessing the grid as a
+ *1 dimentional array and using an offset
+ *with a multiple reliant of the size of the grid
+ */
+bool testDiagUp(int player){ 
+int i, j;
+int count = 0;				
+	for(i = 0,j = 0; i < NUMROW; i++,j++){ 
+		if(grid[i][j] == player){
+			count++;
 		}
 	}
+	if( count == 3) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+bool testDiagDown(int player){
+int i, j;
+int count;
+	for(i = NUMROW - 1, j = 0; i > 0; i--,j++){
+		if(grid[i][j] == player){
+			count++;
+		}			
+	}
+	if( count == 3 ) {
+		return 1;
+	} else {
+		return 0;
+	}		
 return 1;
 }
 
 bool winnerTest(int player){ //return true on 'player' win
 int i;
 	for(i = 0; i < NUMCOL; i++){
-		if(winnerTestCol(i,player))
+		if(testCol(i,player)) {
 			return 1;
+		}
 	}
 	for(i = 0; i < NUMROW; i++){
-		if(winnerTestRow(i,player))
+		if(testRow(i,player)) {
 			return 1;
+		}
 	}
-	if(winnerTestDiagUp(player))
+	if(testDiagUp(player)) {
 		return 1;
-	if(winnerTestDiagDown(player))
+	}
+	if(testDiagDown(player)) {
 		return 1;
+	}
 return 0;
 }
 
